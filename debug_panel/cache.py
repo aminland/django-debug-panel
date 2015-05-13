f4be4050eb6538fba2e71b8290703fee18e2b7d7
Daniel Harding <dharding@living180.net>
2015-05-13 15:38:44 +0100

    Don't use deprecated get_cache() on Django >= 1.7

--- debug_panel/cache.py
+++ debug_panel/cache.py
@@ -1,5 +1,13 @@
-from django.core.cache import get_cache
 from django.core.cache.backends.base import InvalidCacheBackendError
+
+
+try:
+    from django.core.cache import caches
+except ImportError:
+    from django.core.cache import get_cache
+else:
+    def get_cache(alias):
+        return caches[alias]
 
 try:
     cache = get_cache('debug-panel')
